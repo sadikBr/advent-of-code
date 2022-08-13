4b080446759e3c871d521743e26af3679953e97b
sadikBr <sadbrah@gmail.com>
2022-08-13 21:25:23 +0100

    add readLines utility function

--- 2021/day-##/utils.js
+++ 2021/day-##/utils.js
@@ -4,10 +4,16 @@ function read(path) {
   return readFile(path).then((buffer) => buffer.toString());
 }
 
+function readLines(path) {
+  return read(path).then((string) => {
+    return string.split('\n').map((val) => val.trim());
+  });
+}
+
 function makeNumbersArray(path) {
   return read(path).then((string) => {
     return string.split('\n').map((val) => parseInt(val.trim()));
   });
 }
 
-module.exports = { read, makeNumbersArray };
+module.exports = { read, readLines, makeNumbersArray };
